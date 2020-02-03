bbb9c009fe57fdba27b0c1e929c53d301e981df0
Nicola <nicola.ba1989@gmail.com>
2020-02-03 17:58:24 +0100

    [HTML-CSS-JS] | imp

--- js/script.js
+++ js/script.js
@@ -2,13 +2,10 @@ $(document).ready(function() {
     // ***************************
     var year = 2018;
     // Months = are zero indexed, so January is month 0.
-    var month = 0;
+    var month = 11;
     // ***************************
     var url_api = 'https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=' + month;
     request(url_api);
-    response = [];
-    // ***************************
-    builderDay = Handlebars.compile($('#day').html());
     // ***************************
     print(year, month);
     // ***************************
@@ -23,11 +20,8 @@ function request(url) {
             method: "GET",
             success: function (r) {
                 if (r.success) {
-                    $.each(r.response, function(key, value) {
-                        response.push(value);
-                    });
-                    console.log(response);
-                    holidays(response);
+                    console.log(r.response);
+                    holidays(r.response);
                 } else {
                     error();
                 }
@@ -40,7 +34,8 @@ function request(url) {
 }
 // ***************************
 function print(year, month) {
-// Days = with Sunday as 0 and Saturday as 6.
+var builderDay = Handlebars.compile($('#day').html());
+// Days = Sunday as 0 and Saturday as 6.
 var days = moment().year(year).month(month).daysInMonth();
 for (var i = 1; i <= days; i++) {
     var config = {
@@ -51,11 +46,11 @@ for (var i = 1; i <= days; i++) {
     }
 }
 // ***************************
-function holidays(array) {
-for (var i = 0; i < array.length; i++) {
-    if (moment(array[i].date).isValid()) {
+function holidays(resp) {
+for (var i = 0; i < resp.length; i++) {
+    if (moment(resp[i].date).isValid()) {
         for (var x = 0; x < $('.month li').length; x++) {
-            if (moment(array[i].date).date() == $('.month li').eq(x).attr('data')) {
+            if (moment(resp[i].date).date() == $('.month li').eq(x).attr('data')) {
                 $('.month li').eq(x).addClass('holiday');
                 }
             }
